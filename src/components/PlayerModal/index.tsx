5b051562ef6384f89ac38a11087602c296e7d31e
Nitish K <kaflenit@gmail.com>
2023-07-28 15:01:55 -0500

    create player modal and trigger it to ask for player name

---
+++ src/components/PlayerModal/index.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { TPlayerModalProps } from "@/types";
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Input,
+  FormLabel,
+  ModalFooter,
+} from "@chakra-ui/react";
+import Buttons from "../Buttons";
+
+const PlayerModal: React.FC<TPlayerModalProps> = ({
+  isOpen,
+  onClose,
+  onStartGame,
+}) => {
+  const [input, setInput] = useState("");
+
+  const handlePlayerNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const handleStartGame = () => {
+    onStartGame(input);
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Let&apos;s Begin!</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <FormLabel>Player Name:</FormLabel>
+          <Input
+            placeholder="Enter your name"
+            value={input}
+            onChange={handlePlayerNameChange}
+            type="text"
+          />
+        </ModalBody>
+        <ModalFooter
+          display="flex"
+          justifyContent={"center"}
+          alignItems="center"
+        >
+          <Buttons onClick={handleStartGame}>Start</Buttons>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default PlayerModal;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,43 @@
-import { Center, Flex, Heading, Text } from "@chakra-ui/react";
+import { useState } from "react";
+
+import Buttons from "@/components/Buttons";
+import Header from "@/components/Header";
+import PlayerModal from "@/components/PlayerModal";
+import { useGameContext } from "@/context/ContextProvider";
+import Head from "next/head";
 
 export default function Home() {
+  //@ts-ignore
+  const { start, startHandler, setPlayer } = useGameContext();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleStartGame = (playerName: string) => {
+    setPlayer(playerName);
+    startHandler();
+    handleModalClose();
+  };
+
   return (
-    <Flex as="main" direction="column" align="center" justify="center">
-      <Heading>Catch The Duck</Heading>
-      <Text>A classic DUCK HUNT inspired web game!</Text>
-    </Flex>
+    <>
+      <Head>
+        <title>Catch The Duck</title>
+      </Head>
+      <Header />
+      {start ? <div> started </div> : <div> Not started </div>}
+      <Buttons onClick={handleModalOpen}>Play</Buttons>
+      <PlayerModal
+        isOpen={isModalOpen}
+        onClose={handleModalClose}
+        onStartGame={handleStartGame}
+      />
+    </>
   );
 }
