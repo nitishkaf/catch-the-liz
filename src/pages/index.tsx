6917697f9eb6ecb5e7bf18a36de48ca74ab33c27
Nitish K <kaflenit@gmail.com>
2023-07-28 11:34:47 -0500

    default to dark theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
+import { Center, Flex, Heading, Text } from "@chakra-ui/react";
+
 export default function Home() {
-  return <></>;
+  return (
+    <Flex as="main" direction="column" align="center" justify="center">
+      <Heading>Catch The Duck</Heading>
+      <Text>A classic DUCK HUNT inspired web game!</Text>
+    </Flex>
+  );
 }
